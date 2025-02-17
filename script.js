1d5531de17192cd7a36b8fccc0b742d417c45afe
MohamedHediZadem <mohamedhedi.zadem@insat.ucar.tn>
2025-02-17 22:40:47 +0100

    final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let listElements = document.querySelectorAll('#colorList li');
 
+//function to change the background color and the text color
 function randomColor(li) {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
@@ -7,13 +8,13 @@ function randomColor(li) {
     let color = `rgb(${r}, ${g}, ${b})`;
     li.style.backgroundColor = color;
     let brightness = 0.299 * r + 0.587 * g + 0.114 * b;
-    let textColor = brightness > 186 ? "black" : "white";
+    let textColor = brightness > 186 ? "black" : "white"; //for low brightness the text color is black and it turns white for low brightness
     li.style.setProperty('color', textColor, 'important');
     return;
 }
 
 
-
+//adding the event listeners so that on every click the color changes
 listElements.forEach( li =>{
     li.addEventListener('click', ()=>{
         randomColor(li);
